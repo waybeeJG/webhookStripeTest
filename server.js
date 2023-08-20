09a837fe4bad6b9d69519dfdd8a031f703e3f62a
waybeeJG <joao.gabriel@waybee.com.br>
2023-08-19 20:18:15 -0400

    improve log - 1

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream('webhookData.log', {flags : 'w'});
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -9,16 +11,10 @@ app.use(express.json());
 // Your webhook endpoint
 app.post('/webhook', (req, res) => {
   console.log('Received Webhook:', req.body);
+  var jsonObj = JSON.parse(req.body);
 
   // Save to file
-  fs.appendFile('webhookData.log', JSON.stringify(req.body) + '\n', (err) => {
-    if (err) {
-      console.error('Failed to write to file', err);
-      return res.status(500).send('Error saving data');
-    }
-
-    res.status(200).send('Webhook received and saved!');
-  });
+  log_file.write(jsonObj + '\n');
 });
 
 app.listen(PORT, () => {
